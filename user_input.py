9d91983c6f22c5c8e1623d5986bcb48e66feb885
Swa-ibu <128294405+Swa-ibu@users.noreply.github.com>
2024-03-06 11:04:30 +0300

    Create user_input.py
    First Python commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("what is your name: ")
+age = input("what is your age: ")
+location = input("Your location: ")
+
+print("Hello " + name , "your age is " + age + " years old","and you live in " + location +"." )
